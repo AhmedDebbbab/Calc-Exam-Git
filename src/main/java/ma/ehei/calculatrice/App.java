450fc5de58ac14d617c59055fb38312f65c23e08
Salsabil DOUHI <douhisalsabil@gmail.com>
2024-01-04 22:25:13 +0100

    Correction d'addition

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -2,6 +2,9 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(Calculatrice.add(5, 5));
+		System.out.println(Calculatrice.add(35, 5));
+ 		System.out.println(Calculatrice.add(25, 5));			
+		System.out.println(Calculatrice.add(15, 5));
 	}
 }
+
